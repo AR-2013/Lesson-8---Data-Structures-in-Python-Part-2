78b9e08a7bdea89343d01567386334624156f0ca
AR-2013 <anairarandeva@icloud.com>
2025-08-31 12:44:52 +0400

    Set intersection

---
+++ Set Intersection.py
@@ -0,0 +1,8 @@
+setx = {"puppy", "kitten", "dog", "cat"}
+sety = {"puppy", "capybara", "kitten"}
+print("Original set elements :")
+print(setx)
+print(sety)
+print("/nIntersection of two said sets :")
+setz = setx.intersection(sety)
+print(setz)
