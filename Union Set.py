62b4293d19f1743fecc126e00282c400299b1afe
AR-2013 <anairarandeva@icloud.com>
2025-08-31 12:39:38 +0400

    Union Set

---
+++ Union Set.py
@@ -0,0 +1,8 @@
+set1 = {"red", "orange", "yellow"}
+set2 = {"green", "blue", "purple"}
+print("Original sets :")
+print(set1)
+print(set2)
+set = set1.union(set2)
+print("/nUnion of above sets :")
+print(set)
